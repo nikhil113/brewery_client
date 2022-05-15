ed5080a4a8a9934921d724866781b8cdbba2adb3
Nikhil Anand <300074488@192.168.1.4>
2022-05-15 16:31:23 +0530

    initial commit

---
+++ src/test/java/adhyayana/brewery_client/BreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package adhyayana.brewery_client;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
